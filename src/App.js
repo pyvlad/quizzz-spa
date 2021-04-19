c24794d2e1c26f40d4da25e1022482bf3c1a9e27
PyVlad <32043790+pyvlad@users.noreply.github.com>
2021-04-19 21:25:48 +0300

    connect React and backends via "proxy" in package.json and "Time" component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
+const Time = () => {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/api/time/')
+      .then(res => res.json())
+      .then(data => {
+        setCurrentTime(data.time);
+      });
+  }, []);
+
+  return <p>The current time is {(new Date(currentTime)).toTimeString()}.</p>;
+}
+
 
 function App() {
   return (
@@ -17,6 +34,7 @@ function App() {
         >
           Learn React
         </a>
+        <Time />
       </header>
     </div>
   );
