aea4762db4bfc8d22860ed1489696ed03a877afa
PyVlad <32043790+pyvlad@users.noreply.github.com>
2021-07-19 19:27:53 +0300

    drf & deploy: add production deployment

--- drf/settings/production.py
+++ drf/settings/production.py
@@ -2,3 +2,31 @@
 Production environment settings.
 """
 from .base import *
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
+
+DEBUG = False
+
+ALLOWED_HOSTS = [get_secret('DOMAIN'), '.localhost', '127.0.0.1', '[::1]']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quizzz',
+    }
+}
+
+# local send-only postfix server:
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "localhost"
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = False
+EMAIL_TIMEOUT = 1 # seconds for blocking operations (e.g. connection attempt)
+DEFAULT_FROM_EMAIL = f"Quizzz <no-reply@{get_secret('DOMAIN')}>"
+
+
+QUIZZZ_FRONTEND_BASE_URL = f"https://{get_secret('DOMAIN')}"
